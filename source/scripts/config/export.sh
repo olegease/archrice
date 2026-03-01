96fabb911837d02b41fe811acd8a5643ddee8b24
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-03-01 18:17:52 +0200

    scripts progress

--- source/scripts/config/export.sh
+++ source/scripts/config/export.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
-
-read -p "your username: " t_ && export yo_user=$t_ && echo
-[ -z "$yo_user" ] && echo "username cannot be empty" && exit 1
-read -sp "your password: " t_ && export yo_pass=$t_ && unset t_ && echo
-[ -z "$yo_pass" ] && echo "password cannot be empty" && exit 1
-read -sp "confirm password: " t_ && export yo_pass_=$t_ && unset t_ && echo
-
-[ "$yo_pass" != "$yo_pass_" ] && echo "passwords do not match" && exit 1
-unset $yo_pass_
+# user
+# read -p "your username: " t_ && export yo_user=$t_
+# [ -z "$yo_user" ] && echo "username cannot be empty" && exit 1
+# read -sp "your password: " t_ && export yo_pass=$t_ && unset t_
+# [ -z "$yo_pass" ] && echo "password cannot be empty" && exit 1
+# read -sp "reapeat password: " t_ && yo_pass_=$t_ && unset t_ && echo
+# [ "$yo_pass" != "$yo_pass_" ] && echo "user passwords mismatch" && exit 1
+# unset $yo_pass_
+# root
+# read -sp "root password, (root): " t_ && yo_root_=$t_ && unset t_
+# [ -z "$yo_root_" ] && export yo_root="root" || export yo_root=$yo_root_
+# [ "$yo_root" != "root" ] && read -sp "repeat password: " yo_root_ || yo_root_=$yo_root && echo
+# [ "$yo_root" != "$yo_root_" ] && echo "root passwords mismatch" && exit 1
+# unset $yo_root_
+# device
+[ -d /sys/firmware/efi ] && export yo_uefi=efibootmgr
+echo $yo_uefi
+read -p "your device, (/dev/)...: " t_ && export yo_device=/dev/$t_ && unset t_
+export yo_deswap=${yo_device}1
+export yo_depast=${yo_device}2
+export yo_deroot=${yo_device}3
+export yo_deboot=${yo_device}4
+[ -n "$yo_uefi" ] && export yo_deuefi=${yo_device}5
+export yo_decode=${yo_device}6
+export yo_dedata=${yo_device}7
+export yo_dehome=${yo_device}8
+# other
+read -p "your keyboard mappings, (us): " t_ && export yo_keys=$t_ && echo
+[ -z "$yo_keys" ] && yo_keys=us
+read -p "your locale, (en_US.UTF-8): " t_ && export yo_lang=$t_ && echo
+[ -z "$yo_lang" ] && yo_lang=en_US.UTF-8
+read -p "your zone, (Europe/London): " t_ && export yo_zone=$t_ && echo
+[ -z "$yo_zone" ] && yo_zone=Europe/London 
--- source/scripts/install.sh
+++ source/scripts/install.sh
@@ -2,8 +2,21 @@
 
 source config/export.sh
 
-echo $yo_user
-
+echo yo_user: $yo_user
+echo -n yo_pass: && [ -z "$yo_pass" ] && echo " error: password is empty" || echo " ****"
+echo yo_root: "****"
+echo yo_device: $yo_device
+echo yo_deswap: $yo_deswap
+echo yo_depast: $yo_depast
+echo yo_deroot: $yo_deroot
+echo yo_deboot: $yo_deboot
+echo yo_deuefi: $yo_deuefi
+echo yo_decode: $yo_decode
+echo yo_dedata: $yo_dedata
+echo yo_zone: $yo_zone
+echo yo_lang: $yo_lang
+echo yo_keys: $yo_keys
 
 # after user creation
+unset $yo_root
 unset $yo_pass

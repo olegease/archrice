14953ae64d736067e40d870009a2eb38e24fed51
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-05-17 21:32:11 +0300

    update neovim config

--- source/configs/shared/neovim/init.lua
+++ source/configs/shared/neovim/init.lua
@@ -1,5 +1,23 @@
 -- basic options
-vim.cmd('syntax on')
-vim.wo.number = true
+vim.cmd( 'syntax on' )
+---- numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.wo.number = true
+---- tabs
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 8
+-- plugins
+vim.pack.add{
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+}
+---- nvim-lspconfig
+if vim.fn.executable( "clangd" ) == 1 then
+  vim.lsp.config.clangd = {
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp" },
+  }
+  vim.lsp.enable("clangd", {})
+end

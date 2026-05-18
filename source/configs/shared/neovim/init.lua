f778d1236967f9e8776eb899353a135878edaa08
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-05-18 16:46:08 +0300

    update neovim config with better comments and styling

--- source/configs/shared/neovim/init.lua
+++ source/configs/shared/neovim/init.lua
@@ -9,33 +9,36 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 8
----- autocomplete
+-- autocomplete
 vim.o.autocomplete = true
 vim.o.completeopt = 'menu,menuone,noselect'
+-- keymap
 ---- helper to trigger native keys
-local function feed_keys(key_string)
-  local keys = vim.api.nvim_replace_termcodes(key_string, true, true, true)
-  vim.api.nvim_feedkeys(keys, 'n', false)
+local function feed_keys( key_string )
+  local keys = vim.api.nvim_replace_termcodes( key_string, true, true, true )
+  vim.api.nvim_feedkeys( keys, 'n', false )
 end
----- insert mode: Control-Space
-vim.keymap.set('i', '<C-Space>', function()
-  feed_keys('<C-x><C-o>')
-end, { desc = 'Trigger LSP Autocomplete' })
----- normal mode: Space to enter append mode
-vim.keymap.set('n', '<Space>', function()
-  feed_keys('a<C-x><C-o>')
-end, { desc = 'Enter Insert Mode and Trigger LSP Autocomplete' })
----- attachment
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
+---- insert mode: Control-Space to trigger LSP autocomplete
+vim.keymap.set( 'i', '<C-Space>', function( )
+  feed_keys( '<C-x><C-o>' )
+end, { desc = 'Trigger LSP Autocomplete' } )
+---- normal mode: Space to trigger append mode with LSP autocomplete
+vim.keymap.set( 'n', '<Space>', function( )
+  feed_keys( 'a<C-x><C-o>' )
+end, { desc = 'Enter Insert Mode and Trigger LSP Autocomplete' } )
+-- enable autocomplete for LSP
+vim.api.nvim_create_autocmd( 'LspAttach', {
+  callback = function( ev )
     local client = assert( vim.lsp.get_client_by_id( ev.data.client_id ) )
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    if client:supports_method( 'textDocument/completion' ) then
+      vim.lsp.completion.enable( true, client.id, ev.buf, { autotrigger = true } )
     end
   end,
-})
+} )
 -- plugins
----- nvim-lspconfig
+vim.pack.add( { } ) -- TODO? need to add < https://github.com/neovim/nvim-lspconfig >
+-- nvim-lspconfig
+---- C/C++
 if vim.fn.executable( "clangd" ) == 1 then
   vim.lsp.config.clangd = {
     cmd = { "clangd", "--header-insertion=never" },
